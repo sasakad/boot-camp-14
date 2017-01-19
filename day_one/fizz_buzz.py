1767968ceabee9b98c75340b776a6e69441efade
sasakad <dosasaka@gmail.com>
2017-01-19 16:14:34 +0300

    Create fizz_buzz.py

---
+++ day_one/fizz_buzz.py
@@ -0,0 +1,12 @@
+def fizzbuzz(x):
+   
+   #This will print the word if either of the conditions are met.
+    if x % 3 == 0 and x % 5 == 0:
+        return "FizzBuzz"
+    elif x % 3 == 0:
+        return "Fizz"
+    elif x % 5 == 0:
+        return "Buzz"
+    else:
+        return  
+  
