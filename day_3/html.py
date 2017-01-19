33403e40a7e134e8ad9cae250af029c2fe55fb2a
sasakad <dosasaka@gmail.com>
2017-01-19 17:41:06 +0300

    Update html.py

--- day_3/html.py
+++ day_3/html.py
@@ -1,1 +1,24 @@
-docty
+<div class="container">  
+  <form id="contact" action="" method="post">
+    <h3>Contact Form</h3>
+    
+    <fieldset>
+      <input placeholder="Your name" type="text" tabindex="1" required autofocus>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your Email Address" type="email" tabindex="2" required>
+    </fieldset>
+    <input type="radio" name="chooseone" value="HTML"><label for="HTML"> Male</label><br>
+ <input type="radio" name="chooseone" value="HTML"><label for="HTML"> Female</label><br>
+ <input type="radio" name="chooseone" value="HTML"><label for="HTML"> Other</label><br>
+    
+    <fieldset>
+      <textarea placeholder="Message" tabindex="5" required></textarea>
+    </fieldset>
+    <fieldset>
+      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+    </fieldset>
+  </form>
+ 
+  
+</div>
