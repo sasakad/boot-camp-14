17dd770ecfb5928465aeb307b679d06b8ceae5bc
sasakad <dosasaka@gmail.com>
2017-01-19 18:51:09 +0300

    Create car-class.py

---
+++ day_one/car-class.py
@@ -0,0 +1,49 @@
+class Car(object):
+     
+      
+     def __init__(self, name = "General", model = "GM", type = "saloon"):
+	        self.type = type
+	        self.model = model
+	        self.name = name
+	        self.speed = 0
+def  num of doors(self):
+        if not self.name== "Porsche":
+            print "i have Two doors."
+        else:
+            print "I am a Porsche ." 
+            
+          if not self.name=="Koenigsegg":
+            print "i have four doors."
+        else:
+            print "I am a Koenigsegg  ." 
+            
+
+	    def num_of_doors(self):
+	        if self.name == "Porshe" or self.name == "Koenigsegg":
+	            return 2
+	        return 4
+	
+
+	    def num_of_wheels(self):
+	        if self.type != "trailer":
+	            return 4
+	        return 8
+	
+
+	    def is_saloon(self):
+	        if self.type == "saloon":
+	            Print” This is a %%s car.
+
+	        return False
+	
+
+	    def drive(self):
+	        if (self.name == "Mercedes" or self.type == "saloon") and i == 3:
+	            self.speed = 1000
+	        elif (self.type == "trailer" or self.type == "trailer") and i == 7:
+	            self.speed = 77
+	        else:
+	            self.speed = 72 
+	        return 0
+          
+
