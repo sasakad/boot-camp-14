7fde114e900f25bd1d581a2b8c7cca27f53d86d7
sasakad <dosasaka@gmail.com>
2017-01-19 15:38:59 +0300

    Create maxmin.py

---
+++ day_2/maxmin.py
@@ -0,0 +1,10 @@
+#fuction to calculate input output
+
+def find_max_min(numbers):
+integers is list [1,2,3,4]
+if (len(int) .1:
+    if max(numbers) == min(integer)):
+               return [len(integer)]
+    elase
+                return[min(integers), max (numbers
+#this fuction has errors tried debuging to no avail
