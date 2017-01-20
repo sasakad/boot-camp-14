403e04fc29feb6caa984bb4dfbcfd2aeb04085a7
sasakad <dosasaka@gmail.com>
2017-01-20 12:02:26 +0300

    Create html webpage.py

---
+++ day_3/html webpage.py
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+  
+  <body>
+    <div class="header">
+      <div class="container">
+        <h1>The Calvary Chapel</h1>
+        <p>Preaching Christ To the World</p>
+        <a class="btn" href="#">Learn More</a>
+      </div>
+    </div>
+
+    <div class="nav">
+      <div class="container">
+        <ul>
+          <li>Ministries</li>
+          <li>Bulletin</li>
+          <li>Sermons</li>
+          <li>Contacts</li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="main">
+      <div class="container">
+        
+        <h2>The Calvary Chapel</h2>
+        <p>Our main goal is to preach the gospel of Jesus Christ throughout as the mandate that Jesus Christ has given us.We aim through the grace of the Father to help nourish souls and bring more people to the knowledge of the Truth.Our statement is that all glory goes to God and to strive to be like Jesus.We abide by the Five Solus.
+        </p>
+      </div>
+    </div>
+     <ul>
+    <li>
+      <h2>Ministries</h2>
+      <p>We have specific ministries that cater to  a made for the needs of this groups and to use their talents for the glory of God.We hope to make impactful change through this ministries.We have a childrens,young adults,men and women industries among others.</p>
+      <a href="#">Learn More about Ministries</a>
+    </li>
+    </ul>
+    
+    <ul>
+    <li>
+    
+    <h3>Bulletin</h3>
+      <p>This is program of how we have set up our services every sunday and where the various ministries are meeting we also have different articles by the members of the various teams and anticpated activities.</p>
+      <a href="#">Learn More about Bulletins</a>
+    </li>
+    </ul>
+    
+
+    
+  </body>
+</html>
+
